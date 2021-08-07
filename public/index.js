a4e959b7a78c500a27ef3e5bcc508acfc55c1121
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-08-06 23:13:15 -0600

    weather request working

--- public/index.js
+++ public/index.js
@@ -1,32 +1,25 @@
 const surfURL = "http://magicseaweed.com/api/";
-const weatherURL = "api.openweathermap.org/data/2.5/";
-const apiKey = "840d0c2134d72e318ad0909c65cf71ee";
-const input = document.querySelector("#searchField");
-
-console.log(input.value);
+const weatherURL = "http://api.openweathermap.org/data/2.5/";
+const apiKey = config.MY_KEY;
+const input = document.getElementById("searchField");
 
 // const getSurfReport = (event) => {
 //     event.preventDefault()
 
 //     axios.get(`${surfURL}`)
 // }
-const sayHello = () => {
-  console.log("hello");
-};
 
-sayHello();
-
-const getWeather = (e) => {
-  console.log("1");
-  e.preventDefault();
-  console.log("clicked");
-  //   let location = input.value;
-
-  //   axios
-  //     .get(`${weatherURL}weather?q=${location}&appid=${apiKey}`)
-  //     .then((res) => {
-  //       console.log(res.data);
-  // });
+const getWeather = () => {
+  let location = input.value;
+  console.log(location);
+  axios
+    .get(`${weatherURL}weather?q=${location}&appid=${apiKey}&units=imperial`)
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => {
+      console.log("Error: ", err);
+    });
 };
 
 // input.addEventListener("submit", getWeather);
